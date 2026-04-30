fae8dae6f420d3a48954d5bd62a367240687db4e
malak <malakezzengui8@gmail.com>
2026-04-30 17:50:41 +0100

    feat(modal) adding details modal for each card

--- src/App.jsx
+++ src/App.jsx
@@ -32,17 +32,23 @@ function App() {
       rating: 3,
       image:
         "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p18307_p_v8_an.jpg",
+        description:"Matilda (1996) is a beloved family-comedy fantasy directed by Danny DeVito based on Roald Dahl's book. It follows Matilda Wormwood, a brilliant young girl neglected by her selfish parents and bullied by her school's tyrannical principal"
     },
-    {
-      id: 4,
-      title: "Azur & Asmar",
-      rating: 3,
-      image: "https://pad.mymovies.it/filmclub/2006/05/236/locandina.jpg",
-    },
+     {
+    id: -1,
+    title: "Your Name",
+    year: 2016,
+    genre: "Anime / Romance",
+    rating: 5,
+    image: "https://upload.wikimedia.org/wikipedia/en/0/0b/Your_Name_poster.png",
+    description:
+      "Two teenagers mysteriously swap bodies and form a deep connection across time and space.",
+  },
   ]);
 
   const bestMovie = [...movies].sort((a, b) => b.rating - a.rating)[0];
   const top3 = [...movies].sort((a, b) => b.rating - a.rating).slice(0, 3);
+  const [selected,setSelected]=useState(null);
   console.log(top3);
 
   return (
@@ -67,13 +73,12 @@ function App() {
           <p className="label">___ TOP RATED FILM</p>
           <h1>{bestMovie.title}</h1>
           <h2>{bestMovie.year}</h2>
-
           <div className="stars">{"⭐".repeat(bestMovie.rating)}</div>
 
           <p className="desc">{bestMovie.description}</p>
           <div className="buttons">
             <button className="trailer_CTA">Watch Trailer</button>
-            <button className="details_CTA" onClick={() => setOpen(true)}>
+            <button className="details_CTA" onClick={() => setSelected(bestMovie)}>
               View Details
             </button>
           </div>
@@ -87,10 +92,9 @@ function App() {
       {/* TOP RATED */}
       <div className="top-rated">
         <h2>Top Rated</h2>
-
         <div className="top-list">
           {top3.map((m, i) => (
-            <div key={m.id} className="top-card">
+            <div key={m.id} className="top-card" onClick={()=>setSelected(m)}>
               <h3>0{i + 1}</h3>
               <img src={m.image} alt="" />
             </div>
@@ -98,23 +102,33 @@ function App() {
         </div>
       </div>
 
-      {open && (
+      {selected && (
         <div className="overlay">
           <div className="window details-card">
-            <button className="close-modal" onClick={() => setOpen(false)}>
+            <button className="close-modal" onClick={() => setSelected(null)}>
               <X />
             </button>
-            <img width={200} src={bestMovie.image} alt="" />
+            <img width={200} src={selected.image} alt="" />
             <div className="details-text">
-              <h1>{bestMovie.title}</h1>
+              <h1>{selected.title}</h1>
               <span>description : </span>
-              <p className="description">{bestMovie.description}</p>
-              <p className="genre">{bestMovie.genre}</p>
-              <p>{bestMovie.rating} / 5 ⭐ </p>
+              <p className="description">{selected.description}</p>
+              <p className="genre">{selected.genre}</p>
+              <p>{selected.rating} / 5 ⭐ </p>
             </div>
           </div>
         </div>
       )}
+      <h1 className="film">All Films</h1>
+      <div className="images">
+      {movies.map((m, i) => (
+        <div key={m.id} className="top-card " onClick={()=>setSelected(m)}>
+              <h3>0{i + 1}</h3>
+              <img src={m.image} alt="" />
+            </div>
+          ))}
+          </div>
+
     </div>
   );
 }
